e46b86e4ad989b47fc411a7823fd3a23cbfa0173
Evgeniya Mikhailova <jane@mobigear.ru>
2015-09-09 13:08:20 +0500

    Переделана entity поста

--- vkTest/CoreData/Post.h
+++ vkTest/CoreData/Post.h
@@ -2,22 +2,31 @@
 //  Post.h
 //  vkTest
 //
-//  Created by Женя Михайлова on 06.09.15.
+//  Created by Женя Михайлова on 08.09.15.
 //  Copyright (c) 2015 jane. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
 #import <CoreData/CoreData.h>
 
+@class Group, NSManagedObject, User;
 
 @interface Post : NSManagedObject
 
-@property (nonatomic, retain) NSNumber * idUnique;
-@property (nonatomic, retain) NSString * userName;
 @property (nonatomic, retain) NSDate * date;
+@property (nonatomic, retain) NSNumber * idUnique;
 @property (nonatomic, retain) NSString * postText;
 @property (nonatomic, retain) NSString * postTitle;
-@property (nonatomic, retain) NSData * postImages;
-@property (nonatomic, retain) NSString * userImageUrl;
+@property (nonatomic, retain) Group *postGroup;
+@property (nonatomic, retain) User *postUser;
+@property (nonatomic, retain) NSSet *attachments;
+@end
+
+@interface Post (CoreDataGeneratedAccessors)
+
+- (void)addAttachmentsObject:(NSManagedObject *)value;
+- (void)removeAttachmentsObject:(NSManagedObject *)value;
+- (void)addAttachments:(NSSet *)values;
+- (void)removeAttachments:(NSSet *)values;
 
 @end
--- vkTest/CoreData/Post.m
+++ vkTest/CoreData/Post.m
@@ -2,21 +2,23 @@
 //  Post.m
 //  vkTest
 //
-//  Created by Женя Михайлова on 06.09.15.
+//  Created by Женя Михайлова on 08.09.15.
 //  Copyright (c) 2015 jane. All rights reserved.
 //
 
 #import "Post.h"
+#import "Group.h"
+#import "User.h"
 
 
 @implementation Post
 
-@dynamic idUnique;
-@dynamic userName;
 @dynamic date;
+@dynamic idUnique;
 @dynamic postText;
 @dynamic postTitle;
-@dynamic postImages;
-@dynamic userImageUrl;
+@dynamic postGroup;
+@dynamic postUser;
+@dynamic attachments;
 
 @end
